8120f6a4ce56646f09863324c574f35ad043acef
chester-rgb <chester@adholic.com.tw>
2026-05-25 15:52:37 +0800

    先拿掉咖啡豆

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <Hero />
       <Showcase />
       <Features />
-      <CoffeeBeansCard />
+      {/* <CoffeeBeansCard /> */}
       <CTA />
       <Footer />
     </main>
